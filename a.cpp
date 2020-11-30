066a9d7f968dd65ed19acc580cbfec598881559d
icoder211 <58630967+icoder211@users.noreply.github.com>
2020-11-30 21:24:22 +0530

    99th Educational Round

---
+++ a.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ln "\n"
+#define pb push_back
+typedef long double ld;
+typedef long long ll;
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+
+void go() {
+  string s;cin >> s;
+  
+
+}
+
+int main() {
+  IO;
+  int t;cin >> t;
+  while(t--) go();
+}
+
+
+
+
+
+
+
