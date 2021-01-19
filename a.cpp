a54d490d7100627502c21603d72b9559b62306ff
icoder211 <58630967+icoder211@users.noreply.github.com>
2021-01-19 12:09:55 +0530

    .

--- a.cpp
+++ a.cpp
@@ -10,141 +10,63 @@ using namespace std;
 typedef long double ld;
 typedef long long ll;
 
-// string s;
-// ll x, y;
-// void go() {
-//   cin >> s;
-//   cin >> x >> y;
-//   ll n = s.size();
-//   ll i = s.size()-1;
-//   ll onep=0, zerop=0, one=0, zero = 0;
-//   forn(j,n) {
-//     if(s[j] == '1') one++;
-//     else if(s[j] == '0') zero++;
-//   }
-//   while(i >= 0) {
-//     // cout<<i<<ln;
-//     if(s[i] == '1') {
-//       onep++;
-//       one--;
-//       i--;
-//       continue;
-//     }else if(s[i] == '0') {
-//       zerop++;
-//       zero--;
-//       i--;
-//       continue;
-//     }
-//     // ll j = i;
-//     // while(s[j] == '?') j--;
-//     if(x * (onep - zero) <= y * (zerop - one)) {
-//       while(i >= 0 and s[i] == '?') {
-//         zerop++;
-//         s[i] = '0';
-//         i--;
-//       }
-//     }else {
-//       while(i >= 0 and s[i] == '?') {
-//         onep++;
-//         s[i] = '1';
-//         i--;
-//       }
-//     }
-    
-//   }
-//   ll ans = 0;
-//   forn(i,n) {
-//     if(s[i] == '1') onep--;
-//     else {
-//       ans += x * onep;
-//     }
-//   }
-//   forn(i,n) {
-//     if(s[i] == '0') zerop--;
-//     else {
-//       ans += y * zerop;
-//     }
-//   }
-//   cout << ans << ln;
-// }
+ll n, m;
+vector<vector<pair<ll, ll>>> ad;
+vector<ll> vis;
+vector<ll> path, mx, mn;
+void go() {
+	cin >> n >> m;
+	ad.assign(n, {});
+	vis.assign(n, 0);
+	path.assign(n, 1e16+7);
+	mn.assign(n, 1e9+1);
+	mx.assign(n, -1);
 
-int n;
-vector<pair<int, int> > ans;
-void pt() {
-  cout << ans.size() << ln;
-  for(auto u: ans) {
-    cout << u.first << " " << u.second << ln;
-  }
+	path[0] = 0;
+	mx[0] = 0;
+	mn[0] = 1e9+1;
+
+	forn(i,m) {
+		ll u, v, t;cin >> u >> v >> t;
+		// cout << u << v << t << endl;
+		u--;v--;
+		ad[u].pb({v, t});
+		ad[v].pb({u, t});
+	}
+	priority_queue<pair<ll, ll>> q;
+	q.push({0, 0});
+
+	while(!q.empty()) {
+		auto uu = q.top();
+		q.pop();
+		ll u = uu.second;
+		// cout << "u " << u << endl;
+		if(vis[u]) continue;
+
+		for(pair<ll, ll> yy: ad[u]) {
+			if(vis[yy.first]) continue;
+			int y = yy.first;
+			if(path[y] - mx[y] + mn[y] >  path[u] - max(mx[u], yy.second) + min(mn[u], yy.second) + yy.second) {
+				path[y] = path[u] + yy.second;
+				mx[y] = max(mx[u], yy.second);
+				mn[y] = min(mn[u], yy.second);
+				q.push({- (path[y] - mx[y] + mn[y]), y});
+			}
+		}
+		vis[u] = 1;
+	}
+	forsn(i, 1, n) {
+		cout << path[i] - mx[i] + mn[i] << " ";
+	}
+	cout << ln;
 }
-int a[200005];
-void go(){
-  cin >> n;
-  ans.clear();
-  forn(i,n+1) a[i]=i;
-  int p = n;
-  if(n == 3) {
-    ans.pb({3,2});
-    ans.pb({3,2});
-    pt();
-    return;
-  }
-  if(n == 4) {
-    ans.pb({3,2});
-    ans.pb({3,2});
-    ans.pb({4,2});
-    ans.pb({4,2});
-    pt();
-    return;
 
-  }
-  if(n == 5) {
-    ans.pb({3,2});
-    ans.pb({3,2});
-    ans.pb({4,2});
-    ans.pb({4,2});
-    ans.pb({5,2});
-    ans.pb({5,2});
-    ans.pb({5,2});
-    pt();
-    return;
-
-  }
-  int i = n-1;
-  while(p > 2 and i > 0) {
-    // cout << p << ln;
-    if(i <= sqrt(p) + 2)  break;
-    while(i > sqrt(p)+2) {
-      ans.pb({i, p});
-      a[i] = (a[i] + a[p] - 1) / a[p];
-      i--;
-    }
-    ans.pb({p, i});
-    ans.pb({p, i});
-    a[p] = (a[p] + a[i] -1) / a[i];
-    a[p] = (a[p] + a[i] -1) / a[i];
-    if(a[p] > 1) {
-      ans.pb({p, i});
-      a[p] = (a[p] + a[i] -1) / a[i];
-
-    }
-    p = i;
-    i--;
-  }
-  forsn(i,3,n+1) {
-    while(a[i] > 1){
-      ans.pb({i, 2});
-      a[i] = (a[i] + 1 ) / 2;
-    }
-  }
-  pt();
-  
-}
-    
-  
  
 int main() {
-  IO;
-  int t;cin >> t;
-  while(t--) 
-  go();
+    IO;
+    
+	// int t;cin >> t;
+	// while(t--)
+	go();
+	
 }
