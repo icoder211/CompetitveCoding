514b3898672454395666a758a0791a9bb3f39696
icoder211 <58630967+icoder211@users.noreply.github.com>
2021-01-21 13:25:03 +0530

    .

--- a.cpp
+++ a.cpp
@@ -9,64 +9,75 @@ using namespace std;
  
 typedef long double ld;
 typedef long long ll;
-
-ll n, m;
-vector<vector<pair<ll, ll>>> ad;
-vector<ll> vis;
-vector<ll> path, mx, mn;
-void go() {
-	cin >> n >> m;
-	ad.assign(n, {});
-	vis.assign(n, 0);
-	path.assign(n, 1e16+7);
-	mn.assign(n, 1e9+1);
-	mx.assign(n, -1);
-
-	path[0] = 0;
-	mx[0] = 0;
-	mn[0] = 1e9+1;
-
-	forn(i,m) {
-		ll u, v, t;cin >> u >> v >> t;
-		// cout << u << v << t << endl;
-		u--;v--;
-		ad[u].pb({v, t});
-		ad[v].pb({u, t});
-	}
-	priority_queue<pair<ll, ll>> q;
-	q.push({0, 0});
-
-	while(!q.empty()) {
-		auto uu = q.top();
-		q.pop();
-		ll u = uu.second;
-		// cout << "u " << u << endl;
-		if(vis[u]) continue;
-
-		for(pair<ll, ll> yy: ad[u]) {
-			if(vis[yy.first]) continue;
-			int y = yy.first;
-			if(path[y] - mx[y] + mn[y] >  path[u] - max(mx[u], yy.second) + min(mn[u], yy.second) + yy.second) {
-				path[y] = path[u] + yy.second;
-				mx[y] = max(mx[u], yy.second);
-				mn[y] = min(mn[u], yy.second);
-				q.push({- (path[y] - mx[y] + mn[y]), y});
-			}
+ 
+int n;
+int a[2002];
+set<int> s;
+vector<pair<int, int>> ans;
+bool f(int i, int p) {
+	if(i < 0) return true;
+	if(s.count(i)) return f(i-1, p);
+	int st = lower_bound(a, a+i, p - a[i]) - a;
+	if(st >= 0 and st < i and a[st] == p - a[i]) {
+		// good
+		int ed = upper_bound(a, a+i, p - a[i]) - a;
+		int j = st;
+		while(j < ed and s.count(j)) {
+			j++;
 		}
-		vis[u] = 1;
+		if(j == ed or j == i) return false;
+		s.insert(j);
+		ans.pb({j, i});
+		return f(i-1, a[i]);
+	}else {
+		return false;
 	}
-	forsn(i, 1, n) {
-		cout << path[i] - mx[i] + mn[i] << " ";
-	}
-	cout << ln;
+ 
 }
-
+void go() {
+	cin >> n;
+	int cnt[1000006] = {0};
+ 
+	bool ok = true;
+	forn(i, 2*n) {
+		cin >> a[i];
+		cnt[a[i]]++;
+		if(cnt[a[i]] > 100) ok = false; 
+	}
+	sort(a, a+2*n);
+	if(!ok) {
+		cout << "NO\n";
+		return;
+	}
+	
+	forn(i,2*n - 1) {
+		// cout << i << ln;
+		ans.clear();
+		s.clear();
+		if(f(2*n-1, a[2*n-1] + a[i])) {
+			cout << "YES\n";
+			cout << a[ans[0].first] + a[ans[0].second] << ln;
+			for(auto u: ans) {
+				cout << a[u.first] << " " << a[u.second] << ln;
+			}
+			return;
+		}
+		// else if(!ans.empty()) {
+		// 	cout << a[ans[0].first] + a[ans[0].second] << ln;
+		// 	for(auto u: ans) {
+		// 		cout << a[u.first] << " " << a[u.second] << ln;
+		// 	}
+		// }
+	}
+	cout << "NO\n";
+}
  
 int main() {
     IO;
     
-	// int t;cin >> t;
-	// while(t--)
+	int t;cin >> t;
+	while(t--) 
 	go();
+	
 	
 }
