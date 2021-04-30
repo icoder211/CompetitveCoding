a97dc353b5e1a932fd7f82b9de659381524bb5ad
icoder211 <58630967+icoder211@users.noreply.github.com>
2021-04-30 12:42:02 +0530

    .

--- b.cpp
+++ b.cpp
@@ -46,7 +46,7 @@ ll gcd(ll c, ll d, ll &x, ll &y) {
 	return g;
 }
  
-const ll N = 5050;
+const ll N = 202020;
 // vector<ll> pr;
 // vector<ll> lp;
 // // vector<int> g[N];
@@ -69,73 +69,112 @@ const ll N = 5050;
 // }
  
  
-ll mod = 1e9+7;
-// ll mod = 998244353;
-ll n;
-ll u[N], s[N], ans[N];
-vector<ll> a[N];
-pair<ll, vector<ll> > b[N];
-// bool cmp(const vector<ll> &a1, const vector<ll> &a2) {
-// 	return a1.size() >= a2.size();
+// ll mod = 1e9+7;
+// // ll mod = 998244353;
+// ll n;
+// ll u[N], s[N], ans[N];
+// vector<ll> a[N];
+// // pair<ll, vector<ll> > b[N];
+// // bool cmp(const vector<ll> &a1, const vector<ll> &a2) {
+// // 	return a1.size() >= a2.size();
+// // }
+// ll mxind=-1;
+// void go() {
+// 	cin >> n;
+// 	forn(i,n) {
+// 		cin >> u[i];u[i]--;
+// 	}
+// 	// ll anss = 0;
+// 	forn(i,n+1) ans[i] = 0;
+// 	vector<ll> sum;
+// 	sum.assign(n,0);
+// 	forn(i,n) a[i].clear();
+// 	forn(i,n) {
+// 		cin >> s[i];
+// 		a[u[i]].pb(s[i]);
+// 		sum[u[i]] += s[i];
+// 		// b[u[i]].second.pb(s[i]);
+// 		// anss += s[i];
+// 	}
+// 	forn(i,n) {
+// 		// b[i].first = b[i].second.size();
+// 		sort(all(a[i])); reverse(all(a[i]));
+// 		// sort(all(b[i].second)); reverse(all(b[i].second));
+// 	}
+// 	// sort(b,b+n);
+// 	// forn(i,n) a[i] = b[i].second;
+// 	forn(i,n) {
+// 		forsn(it, 1, a[i].size()+1) {
+// 			ans[it] += sum[i];
+// 			forn(itt, a[i].size()%it) ans[it] -= a[i][a[i].size()-itt-1];
+// 		}
+// 	}
+// 	// ll ind = n-1;
+// 	// forsn(i,1,n+1) {
+// 	// 	while(ind >= 0 and i > a[ind].size()) {
+// 	// 		anss -= sum[ind];
+// 	// 		ind--;
+// 	// 	}
+// 	// 	ans[i] = anss;
+// 	// }
+// 	// forsn(i, 1, n+1) {
+// 	// 	forn(j, n) {
+// 	// 		if(i >= a[j].size()) {break;}
+// 	// 		ll p = ((ll)a[j].size()) % i;
+// 	// 		if(p==0) continue;
+// 	// 		ans[i] -= sums[j][a[j].size() - p];
+// 	// 	}
+// 	// }
+// 	forsn(i,1,n+1) cout << ans[i] << " ";
+// 	cout << ln;
 // }
-ll mxind=-1;
-void go() {
-	cin >> n;
-	forn(i,n) {
-		cin >> u[i];u[i]--;
+
+int n,m;
+vector<int> dp[101][10101];
+int ok[101][10101];
+vector<int> ans;
+vector<int> f(int n, int m) {
+	if(m == n*n) {
+		ans.pb(n);
+		dp[n][m].clear();
+		dp[n][m].pb(n);
+		return dp[n][m];
 	}
-	ll anss = 0;
-	forn(i,n) a[i].clear();
-	forn(i,n) {
-		cin >> s[i];
-		a[u[i]].pb(s[i]);
-		// b[u[i]].second.pb(s[i]);
-		anss += s[i];
-	}
-	forn(i,n) {
-		// b[i].first = b[i].second.size();
-		sort(all(a[i])); reverse(all(a[i]));
-		// sort(all(b[i].second)); reverse(all(b[i].second));
-	}
-	// sort(b,b+n);
-	// forn(i,n) a[i] = b[i].second;
-	auto cmp = [](vector<ll> a, vector<ll> b) { return a.size() > b.size(); };
-	sort(a,a+n,cmp);
-	vector<ll> sum;
-	vector<vector<ll>> sums;
-	forn(i,n) {
-		sum.pb(0);
-		sums.pb({});
-		// for(auto u: a[i]) sum[i] += u;
-		if(a[i].size()==0)continue;
-		sums[i].assign(a[i].size(), 0);
-		sums[i][a[i].size()-1] = a[i].back();
-		for(int j = a[i].size()-2;j >= 0;j--) {
-			sums[i][j] = sums[i][j+1] + a[i][j];
+	if(m < 0) return {};
+	if(ok[n][m] >= 0) return dp[n][m];
+	int it = 1;
+	// dp[n][m] = {};
+	int itt=-1;
+	while(it < n) {
+		if(it*it >= m) break;
+		vector<int> p = f(n- it, m- it*it);
+		if(p.size()==0) continue;
+		if( itt == -1 or dp[n][m].size() > p.size() ) {
+			dp[n][m] = p;
+			itt = it;
 		}
-		if(a[i].size() > 0) sum[i] = sums[i][0];
+		it++;
 	}
-	ll ind = n-1;
-	forsn(i,1,n+1) {
-		while(ind >= 0 and i > a[ind].size()) {
-			anss -= sum[ind];
-			ind--;
-		}
-		ans[i] = anss;
-	}
-	forsn(i, 1, n+1) {
-		forn(j, n) {
-			if(i >= a[j].size()) {break;}
-			ll p = ((ll)a[j].size()) % i;
-			if(p==0) continue;
-			ans[i] -= sums[j][a[j].size() - p];
-		}
-	}
-	forsn(i,1,n+1) cout << ans[i] << " ";
-	cout << ln;
+	if(itt > -1) {
+		dp[n][m].pb(itt);
+		ok[n][m] = 1;
+	} else ok[n][m] = 0;
+	return dp[n][m];
 }
+void go1() {
+	ans.clear();
+	cin >> n >> m;
+	if(m > n*n) {cout<<-1<<ln;return;}
+	vector<int> p = f(n,m);
+	if(ok[n][m]) {
+		ans = dp[n][m];
+		cout << ans.size() << ln;
+		forn(i,ans.size()) cout << ans[i] << " ";
+		cout << ln;
+	}else cout << -1 << ln;
 
 
+}
 
 
 int main() {
@@ -147,8 +186,10 @@ int main() {
 	int t; t = 1;
     cin >> t;
 	int tt = t;
+	forn(i,101) forn(j,10001) ok[i][j] = -1;
     while(t--) {
 		// cout << "Case #"<<tt-t<<": ";
-		go();
+		go1();
 	}
+	// forn(i,10) {forn(j,10) cout << dp[i][j] << " ";cout << ln;}
 }
